3cf6d1ad2deaffaa7f6eb87203af7675265bfe08
Philipp Bosch <hello@pb.io>
2011-11-07 11:43:25 +0100

    Use relative paths for assets.

--- config.rb
+++ config.rb
@@ -7,6 +7,7 @@ css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
+relative_assets = true
 
 # output_style = :expanded or :nested or :compact or :compressed
 output_style = :compressed
